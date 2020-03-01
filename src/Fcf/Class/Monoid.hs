23ab511423e72f4386f467226f9735a80b61d709
Lysxia <lysxia@gmail.com>
2020-03-01 13:34:07 -0500

    Add instances of (<>) and MEmpty

--- src/Fcf/Class/Monoid.hs
+++ src/Fcf/Class/Monoid.hs
@@ -9,10 +9,12 @@
 -- | Semigroups and monoids.
 module Fcf.Class.Monoid
   ( -- * Pure type families
+    -- | Nicer to use when applied explicitly.
     type (<>)
   , MEmpty
 
     -- * First-class families
+    -- | Can be composed and passed to higher-order functions.
   , type (.<>)
   , MEmpty_
   ) where
@@ -29,6 +31,12 @@ type instance Eval (x .<> y) = x <> y
 -- | Type-level semigroup composition @('Data.Semigroup.<>')@.
 type family (<>) (x :: a) (y :: a) :: a
 
+-- (,)
+type instance (<>) '(a1, a2) '(b1, b2) = '(a1 <> b1, a2 <> b2)
+
+-- (,,)
+type instance (<>) '(a1, a2, a3) '(b1, b2, b3) = '(a1 <> b1, a2 <> b2, a3 <> b3)
+
 -- List
 type instance (<>) '[] ys = ys
 type instance (<>) (x ': xs) ys = x ': (<>) xs ys
@@ -44,6 +52,8 @@ type instance (<>) a 'EQ = a
 type instance (<>) 'LT _b = 'LT
 type instance (<>) 'GT _b = 'GT
 
+-- ()
+type instance (<>) _a _b = '()
 
 -- | Type-level monoid identity 'Data.Monoid.mempty'.
 --
@@ -54,6 +64,12 @@ type instance Eval MEmpty_ = MEmpty
 -- | Type-level monoid identity 'Data.Monoid.mempty'.
 type family MEmpty :: a
 
+-- (,)
+type instance MEmpty = '(MEmpty, MEmpty)
+
+-- (,,)
+type instance MEmpty = '(MEmpty, MEmpty, MEmpty)
+
 -- List
 type instance MEmpty = '[]
 
@@ -62,3 +78,6 @@ type instance MEmpty = 'Nothing
 
 -- Ordering
 type instance MEmpty = 'EQ
+
+-- ()
+type instance MEmpty = '()
